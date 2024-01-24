46015066a294483e0f28e25e59ad04e957aedee6
X-SLAYER <slayertn1999@gmail.com>
2024-01-24 23:11:47 +0100

    Add checkbox logic

--- src/app/components/Controls.jsx
+++ src/app/components/Controls.jsx
@@ -1,38 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Controls({}) {
+export default function Controls({ onControlChange }) {
+  const [controls, setControls] = useState({
+    genJson: false,
+    genCopyWith: false,
+    genToString: false,
+    genJsonKeys: false,
+    useNumForNumber: false,
+    useSerializable: false,
+    useEquatable: false,
+    useDefaultValue: false,
+    useRequiredFields: false,
+  });
+
+  const handleCheckboxChange = (event) => {
+    const { id, checked } = event.target;
+    console.log(id, checked);
+    setControls((prevControls) => {
+      const newControls = { ...prevControls, [id]: checked };
+      onControlChange(newControls);
+      return newControls;
+    });
+  };
+
+  useEffect(() => {
+    onControlChange(controls);
+  }, []);
+
   return (
     <div className="mb-4">
       <div id="check-group" className="flex flex-wrap -mx-2">
         <div className="flex flex-col w-full px-2 sm:w-1/2">
           <label className="flex items-center space-x-2">
-            <input type="checkbox" className="settings-checkbox" id="gen-json" />
+            <input
+              type="checkbox"
+              className="settings-checkbox"
+              id="gen-json"
+              onClick={handleCheckboxChange}
+            />
             <span>
               Generate <span className="font-mono">toJson</span> method
             </span>
           </label>
 
           <label className="flex items-center space-x-2">
-            <input type="checkbox" className="settings-checkbox " id="gen-cpy" />
+            <input
+              type="checkbox"
+              className="settings-checkbox "
+              id="gen-cpy"
+              onClick={handleCheckboxChange}
+            />
             <span>
               Generate <span className="font-mono">copyWith</span> method
             </span>
           </label>
 
           <label className="flex items-center space-x-2">
-            <input type="checkbox" className="settings-checkbox" id="gen-ts" />
+            <input
+              type="checkbox"
+              className="settings-checkbox"
+              id="gen-ts"
+              onClick={handleCheckboxChange}
+            />
             <span>
               Generate <span className="font-mono">toString</span> method
             </span>
           </label>
 
           <label className="flex items-center space-x-2">
-            <input type="checkbox" className="settings-checkbox" id="gen-key" />
+            <input
+              type="checkbox"
+              className="settings-checkbox"
+              id="gen-key"
+              onClick={handleCheckboxChange}
+            />
             <span>Generate JSON keys</span>
           </label>
 
           <label className="flex items-center space-x-2">
-            <input type="checkbox" className="settings-checkbox" id="use-num" />
+            <input
+              type="checkbox"
+              className="settings-checkbox"
+              id="use-num"
+              onClick={handleCheckboxChange}
+            />
             <span>
               Always use <span className="font-mono">num</span> type for number
             </span>
@@ -45,6 +96,7 @@ export default function Controls({}) {
               type="checkbox"
               className="settings-checkbox"
               id="use-serializable"
+              onClick={handleCheckboxChange}
             />
             <span>
               Use <span className="font-mono">JSONSerializable</span>
@@ -56,6 +108,7 @@ export default function Controls({}) {
               type="checkbox"
               className="settings-checkbox"
               id="use-equatable"
+              onClick={handleCheckboxChange}
             />
             <span>
               Use <span className="font-mono">Equatable</span>
@@ -67,6 +120,7 @@ export default function Controls({}) {
               type="checkbox"
               className="settings-checkbox"
               id="use-default-value"
+              onClick={handleCheckboxChange}
             />
             <span>Use default value </span>
           </label>
@@ -76,6 +130,7 @@ export default function Controls({}) {
               type="checkbox"
               className="settings-checkbox"
               id="use-required-fields"
+              onClick={handleCheckboxChange}
             />
             <span>Use required fields</span>
           </label>
--- src/app/editor/page.jsx
+++ src/app/editor/page.jsx
@@ -3,17 +3,24 @@
 import dynamic from "next/dynamic";
 import AppBar from "../components/AppBar";
 import Controls from "../components/Controls";
+import { useState } from "react";
 
 const Editor = dynamic(() => import("../components/Editor"), { ssr: false });
 
 const ProgrammingEditor = () => {
+  const [controlsData, setControlsData] = useState({});
+
+  const handleControlChange = (newControls) => {
+    setControlsData(newControls);
+  };
+
   return (
     <div>
       <AppBar />
       <div className="flex flex-row mx-4">
         <div className="flex flex-col flex-grow w-3/6 p-4 mx-auto mt-3">
-          <Controls />
-          <div className="flex-grow" style={{ height: "70vh" }}>
+          <Controls onControlChange={handleControlChange} />
+          <div className="flex-grow" style={{ height: "60vh" }}>
             <Editor readOnly={false} language="json5" />
           </div>
           <button
@@ -25,10 +32,13 @@ const ProgrammingEditor = () => {
         </div>
 
         <div className="flex flex-col flex-grow w-4/5 p-4 mx-auto mt-3">
-          <div className="flex-grow" style={{ height: "70vh" }}>
+          <div className="flex-grow">
             <Editor language="dart" readOnly={false} />
           </div>
           <button
+            onClick={() => {
+              navigator.clipboard.writeText(JSON.stringify(controlsData));
+            }}
             type="button"
             className="mt-3 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
